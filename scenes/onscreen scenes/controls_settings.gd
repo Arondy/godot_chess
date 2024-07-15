5135a6f9f746be041e92644abd5000575a45614b
arondy <antongatcenko@gmail.com>
2024-07-15 23:21:38 +0300

    Starting menu and settings added

--- scenes/onscreen scenes/controls_settings.gd
+++ scenes/onscreen scenes/controls_settings.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var _controlsVBox = $Margin/VBox
 @onready var _actionKey: Button = _controlsVBox.get_node("Action/Button")
-@onready var _notify = _controlsVBox.get_node("Action/Notification")
+@onready var _remapOverlay = _controlsVBox.get_node("Action/Remap overlay")
 
 func _ready():
 	load_cfg_values()
@@ -13,18 +13,4 @@ func load_cfg_values():
 func _on_button_pressed():
 	if _actionKey.button_pressed:
 		_actionKey.text = "Click any key..."
-		_notify.visible = true
-	else:
-		_actionKey.button_pressed = true
-
-func _unhandled_input(event):
-	if (_actionKey.button_pressed and Input.is_anything_pressed()
-	and ((event is InputEventKey and event.keycode != KEY_ESCAPE)
-	or event is InputEventMouseButton)):
-		InputMap.action_erase_events("left_mouse")
-		InputMap.action_add_event("left_mouse", event)
-		_actionKey.text = event.as_text()
-		_actionKey.button_pressed = false
-		_notify.visible = false
-		Tools.config.set_value("controls", "action", _actionKey.text)
-		get_viewport().set_input_as_handled()
+		_remapOverlay.visible = true
---
+++ scenes/onscreen scenes/remap_overlay.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var _actionKey: Button = $"../Button"
+
+func _input(event):
+	if (_actionKey.button_pressed and Input.is_anything_pressed()
+	and ((event is InputEventKey and event.keycode != KEY_ESCAPE)
+	or event is InputEventMouseButton)):
+		InputMap.action_erase_events("left_mouse")
+		InputMap.action_add_event("left_mouse", event)
+		_actionKey.text = event.as_text()
+		_actionKey.button_pressed = false
+		hide()
+		Tools.config.set_value("controls", "action", _actionKey.text)
+		get_viewport().set_input_as_handled()
